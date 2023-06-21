1bb3cbf3d7476e833a43fbb8db1ca5ad38bcd2f5
Oscar <chikenlitle99@hotmail.com>
2023-06-21 16:26:38 -0600

    First commit

---
+++ src/todo/Todo.tsx
@@ -0,0 +1,12 @@
+export const Todo = () => {
+  return (
+    <>
+      <h1>TODO:</h1>
+      <ul>
+        <li>Todo1</li>
+        <li>Todo2</li>
+        <li>Todo3</li>
+      </ul>
+    </>
+  );
+};
